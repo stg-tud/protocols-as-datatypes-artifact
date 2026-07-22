856c8d26d4a1e05e2d25fcd323f6fe6f2cd8ae6f
Julian <git@julian-haas.de>
2026-07-22 14:16:33 +0200

    fix another typo

--- scripts/all-benchmarks.fish
+++ scripts/all-benchmarks.fish
@@ -3,7 +3,7 @@
 ansible-playbook -i ansible/hCloudInventory.hcloud.yaml -i ansible/inventory.yaml --extra-vars '{"bismuthdir": "/home/julian/Nextcloud/Uni/phd/Darmstadt/REScala", "nodenames": ["node1", "node2", "node3"]}' ansible/create-servers.yaml
 set -x SERVERS 3
 
-fish scripts/data-center-benchmarks.fish
+fish scripts/datacenter-benchmarks.fish
 
 fish scripts/leader-failure-benchmark.fish
 
