6691c29c28b95db2959319aff134e11d5bad850b
Julian <git@julian-haas.de>
2026-07-22 18:38:05 +0200

    100 000 ops for data center local benchmarks...

--- scripts/datacenter-benchmarks.fish
+++ scripts/datacenter-benchmarks.fish
@@ -4,7 +4,7 @@ function run_bench
 	    for system in pb etcd pb etcd pb etcd
 		for threads in 1 2 3 4 5 10 20 50 100 200
 		    echo $workload $system $threads threads
-		    ansible-playbook -i ansible/hCloudInventory.hcloud.yaml -i ansible/inventory.yaml --extra-vars "{\"workload\": \"$workload\", \"threads\": $threads, \"operations\": 10000, \"recordcount\": 1000, \"type\": \"$system\", \"measurementtype\": \"raw\", \"max_batch_size\": 1, \"leader\": \"fsn-node1\", \"benchmark_timeout\": 500, \"leader_timeout\": 50000, \"comment\": \"artifact-eval\", \"commit_reads\": \"false\", \"op_timeout\": \"30\"}" ansible/run-benchmark.yaml
+		    ansible-playbook -i ansible/hCloudInventory.hcloud.yaml -i ansible/inventory.yaml --extra-vars "{\"workload\": \"$workload\", \"threads\": $threads, \"operations\": 100000, \"recordcount\": 1000, \"type\": \"$system\", \"measurementtype\": \"raw\", \"max_batch_size\": 1, \"leader\": \"fsn-node1\", \"benchmark_timeout\": 500, \"leader_timeout\": 50000, \"comment\": \"artifact-eval\", \"commit_reads\": \"false\", \"op_timeout\": \"30\"}" ansible/run-benchmark.yaml
 		end
 	    end
 	end
