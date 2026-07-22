95b4ce9a7fb477722c24190e1837b321e1e31ea4
Julian <git@julian-haas.de>
2026-07-22 14:02:41 +0200

    fix typo in benchmark script

--- scripts/all-benchmarks.fish
+++ scripts/all-benchmarks.fish
@@ -5,13 +5,13 @@ set -x SERVERS 3
 
 fish scripts/data-center-benchmarks.fish
 
-fish scripts/leader-failure-benchmarks.fish
+fish scripts/leader-failure-benchmark.fish
 
 # create 5 servers
 ansible-playbook -i ansible/hCloudInventory.hcloud.yaml -i ansible/inventory.yaml --extra-vars '{"bismuthdir": "/home/julian/Nextcloud/Uni/phd/Darmstadt/REScala", "nodenames": ["node1", "node2", "node3", "node4", "node5"]}' ansible/create-servers.yaml
 set -x SERVERS 5
 
-fish scripts/data-center-benchmarks.fish
+fish scripts/datacenter-benchmarks.fish
 
 # remove servers
 ansible-playbook -i ansible/hCloudInventory.hcloud.yaml -i ansible/inventory.yaml ansible/remove-all-servers.yaml
