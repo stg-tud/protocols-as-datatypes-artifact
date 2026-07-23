df68b3a9252869edb7f3efebfbf377f9abbd4b21
Julian <git@julian-haas.de>
2026-07-23 12:15:17 +0200

    fix bug in leader failure benchmarks

--- scripts/leader-failure-benchmark.fish
+++ scripts/leader-failure-benchmark.fish
@@ -9,7 +9,7 @@ for workload in writeonly
     for system in pb etcd pb etcd pb etcd
         for threads in 1 2 3 20
             echo $workload $system $threads threads
-            ansible-playbook -i ansible/hCloudInventory.hcloud.yaml -i ansible/inventory.yaml --extra-vars "{\"workload\": \"$workload\", \"kill_leader\": 10, \"threads\": $threads, \"operations\": 50000, \"recordcount\": 1000, \"type\": \"$system\", \"measurementtype\": \"raw\", \"max_batch_size\": 1, \"leader\": \"fsn-node1\", \"benchmark_timeout\": 500, \"leader_timeout\": 5000, \"comment\": \"plumtree\", \"commit_reads\": \"false\", \"op_timeout\": \"30\"}" ansible/run-benchmark.yaml
+            ansible-playbook -i ansible/hCloudInventory.hcloud.yaml -i ansible/inventory.yaml --extra-vars "{\"workload\": \"$workload\", \"kill_leader\": 10, \"threads\": $threads, \"operations\": 50000, \"recordcount\": 1000, \"type\": \"$system\", \"measurementtype\": \"raw\", \"max_batch_size\": 1, \"leader\": \"fsn-node1\", \"benchmark_timeout\": 500, \"leader_timeout\": 5000, \"comment\": \"artifact-eval\", \"commit_reads\": \"false\", \"op_timeout\": \"1\"}" ansible/run-benchmark.yaml
         end
     end
 end
