358473c4d2f3ebabee57652e23554106c6596d87
Julian <git@julian-haas.de>
2026-07-24 10:53:10 +0200

    also create PDF when packaging

--- bundle-up.fish
+++ bundle-up.fish
@@ -3,3 +3,8 @@ rm -rf .git .bsp .metals .scala-build .github
 rm -f .gitignore .gitattributes .gitmodules
 rm bundle-up.fish
 zip -r artifact.zip .
+
+cp README.md README_PDF.md
+sed -i '1c ---' README_PDF.md
+
+pandoc -V margin-left=2cm -V margin-right=2cm -V margin-top=2cm -V margin-botton=2cm -V papersize=a4 -o README.pdf --standalone --metadata title="PRDTs: Composable Design and Verification of Consensus Protocols using Replicated Data Types (Artifact)" --highlight-style=kate --toc README_PDF.md
